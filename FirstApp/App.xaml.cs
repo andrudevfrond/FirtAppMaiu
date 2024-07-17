dc9e8a78c7be6b8396e8ab69f8bbd45414530ef8
Andres Galindo <sharlly1329g@gmail.com>
2024-07-17 09:54:18 -0500

    Inicio de proyecto MAUI

---
+++ FirstApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FirstApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
