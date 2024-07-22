d928086f12629c52d61286547308e00c73ce5f1c
Andres Galindo <sharlly1329g@gmail.com>
2024-07-22 09:00:42 -0500

    Arreglo de conexion base datos Sqlite

--- FirstApp/Services/DialogService.cs
+++ FirstApp/Services/DialogService.cs
@@ -5,17 +5,17 @@ namespace FirstApp.Services
     {
         public Task<string> ShowActionAsync(string title, string message, string destruction, params string[] buttons)
         {
-            return  App.Current.MainPage.DisplayActionSheet(title, message, destruction, buttons);
+            return  Application.Current.MainPage.DisplayActionSheet(title, message, destruction, buttons);
         }
 
         public Task<bool> ShowAlertAsync(string title, string message, string accept, string cancel)
         {
-            return App.Current.MainPage.DisplayAlert(title, message, accept, cancel);
+            return Application.Current.MainPage.DisplayAlert(title, message, accept, cancel);
         }
 
         public Task<bool> ShowConfirmAsync(string title, string message)
         {
-            return App.Current.MainPage.DisplayAlert(title, message, "Ok", "No");
+            return Application.Current.MainPage.DisplayAlert(title, message, "Ok", "No");
         }
     }
 }
